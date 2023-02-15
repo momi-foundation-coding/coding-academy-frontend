37aaac01a3c844113b416d4c297205de47e395ff
cheboi <moscheb96gmail.com>
2023-02-15 09:35:54 +0300

    fix not used

--- src/routes/curriculum.js
+++ src/routes/curriculum.js
@@ -2,31 +2,31 @@
 import MainContent from "../pages/Content";
 import TableOfContent from "../pages/TableOfContent";
 
-const data = [
-  {
-    week1: {
-      "Computer science concepts": [],
-      "Role of computers in today’s world": [],
-      "Computer architecture": [],
-      "E-commerce": [],
-      "Computer-related jobs": [],
-    },
-    week2: {
-      "Introduction to programming": [],
-      "Introduction to web design and development": [],
-      "Introduction to mobile development": [],
-    },
-    week3: {
-      "Version control": [],
-      "Introduction to GitHub": [],
-      "Introduction to databases": [],
-    },
-    week4: {
-      "Computer security": [],
-      "Introduction to desktop application": [],
-    },
-  },
-];
+// const data = [
+//   {
+//     week1: {
+//       "Computer science concepts": [],
+//       "Role of computers in today’s world": [],
+//       "Computer architecture": [],
+//       "E-commerce": [],
+//       "Computer-related jobs": [],
+//     },
+//     week2: {
+//       "Introduction to programming": [],
+//       "Introduction to web design and development": [],
+//       "Introduction to mobile development": [],
+//     },
+//     week3: {
+//       "Version control": [],
+//       "Introduction to GitHub": [],
+//       "Introduction to databases": [],
+//     },
+//     week4: {
+//       "Computer security": [],
+//       "Introduction to desktop application": [],
+//     },
+//   },
+// ];
 
 const Curriculum = () => {
   // console.log({ data });
