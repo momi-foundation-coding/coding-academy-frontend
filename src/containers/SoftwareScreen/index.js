c6e06cdd93d89377cf8b46365b8985279f1bd50a
ezkemboi <ezrqnkemboi@gmail.com>
2023-02-25 03:48:48 +0300

    finish up software page

--- src/containers/SoftwareScreen/index.js
+++ src/containers/SoftwareScreen/index.js
@@ -37,6 +37,7 @@ const SoftwareScreen = () => {
             <li>Full-stack Engineer</li>
             <li>Back-end Engineer</li>
             <li>Front-end Engineer</li>
+            <li>Quality Assurance Engineer</li>
             <li>DevOps Engineer</li>
             <li>Project Manager</li>
             <li>Product Manager</li>
@@ -74,6 +75,186 @@ const SoftwareScreen = () => {
             <li>Learn how to work in a team, motivate and lead</li>
           </ul>
         </div>
+      </div>
+      <div
+        className='container-fluid border-top mb-4 align-items-center p-4'
+      >
+        <h3 className='text-center text-success'>Our Programs</h3>
+        <div className="row mb-2">
+          <div className="col-md-4">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                <h5><strong className="d-inline-block mb-2 text-success">Front-end Engineer</strong></h5>
+                <div className="mb-1 text-muted">Area of Learning</div>
+                <li>Computer Science Fundamentals</li>
+                <li>C programming</li>
+                <li>Introduction to Python Programming</li>
+                <li>Data Structures and Algorithms</li>
+                <li>Git & Shell Scripting</li>
+                <li>SQL and NoSQL Databases</li>
+                <li>Itroduction to Web Development</li>
+                <li>Learn HTML and CSS</li>
+                <li>Learn Javascript</li>
+                <li>Learn React</li>
+                <li>Backend Development with Flask</li>
+                <li>Backend Development with NodeJS</li>
+                <button className="btn btn-success col-md-6 mt-4">Check Path</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                <h5><strong className="d-inline-block mb-2 text-success">Back-end Engineer</strong></h5>
+                <div className="mb-1 text-muted">Area of Learning</div>
+                <li>Computer Science Fundamentals</li>
+                <li>C programming</li>
+                <li>Introduction to Python Programming</li>
+                <li>Data Structures and Algorithms</li>
+                <li>Git & Shell Scripting</li>
+                <li>SQL and NoSQL Databases</li>
+                <li>Itroduction to Web Development</li>
+                <li>Learn HTML and CSS</li>
+                <li>Learn Javascript</li>
+                <li>Learn React</li>
+                <li>Backend Development with Flask</li>
+                <li>Backend Development with NodeJS</li>
+                <button className="btn btn-success col-md-6 mt-4">Check Path</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                <h5><strong className="d-inline-block mb-2 text-success">DevOps Engineer</strong></h5>
+                <div className="mb-1 text-muted">Area of Learning</div>
+                <li>Computer Science Fundamentals</li>
+                <li>C programming</li>
+                <li>Introduction to Python Programming</li>
+                <li>Data Structures and Algorithms</li>
+                <li>Git & Shell Scripting</li>
+                <li>SQL and NoSQL Databases</li>
+                <li>Itroduction to Web Development</li>
+                <li>Learn HTML and CSS</li>
+                <li>Learn Javascript</li>
+                <li>Learn React</li>
+                <li>Backend Development with Flask</li>
+                <li>Backend Development with NodeJS</li>
+                <button className="btn btn-success col-md-6 mt-4">Check Path</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                <h5><strong className="d-inline-block mb-2 text-success">Project Manager</strong></h5>
+                <div className="mb-1 text-muted">Area of Learning</div>
+                <li>Agile Project Management</li>
+                <li>Quality Control</li>
+                <li>Communication</li>
+                <li>Change Management</li>
+                <li>Risk Management</li>
+                <li>Strategic Alignment</li>
+                <li>Stakeholder Management</li>
+                <li>resource Management</li>
+                <button className="btn btn-success col-md-6 mt-4">Check Path</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                <h5><strong className="d-inline-block mb-2 text-success">Product Manager</strong></h5>
+                <div className="mb-1 text-muted">Area of Learning</div>
+                <li>Agile Project Management</li>
+                <li>Quality Control</li>
+                <li>Communication</li>
+                <li>Change Management</li>
+                <li>Risk Management</li>
+                <li>Strategic Alignment</li>
+                <li>Stakeholder Management</li>
+                <li>resource Management</li>
+                <button className="btn btn-success col-md-6 mt-4">Check Path</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div className="col p-4 d-flex flex-column position-static">
+                <h5><strong className="d-inline-block mb-2 text-success">Quality Assurance Engineer</strong></h5>
+                <div className="mb-1 text-muted">Area of Learning</div>
+                <li>Agile Project Management</li>
+                <li>Quality Control</li>
+                <li>Communication</li>
+                <li>Change Management</li>
+                <li>Risk Management</li>
+                <li>Strategic Alignment</li>
+                <li>Stakeholder Management</li>
+                <li>resource Management</li>
+                <button className="btn btn-success col-md-6 mt-4">Check Path</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className='container-fluid border-top mb-4 align-items-center p-4'
+      >
+        <h3 className='text-center text-success'>Here From Our Community</h3>
+        <div className="d-flex justify-content-around row mb-2">
+          <div className="col-md-3">
+            <div className="row border p-2 rounded flex-md-row mb-4 position-relative">
+              <strong className="d-inline-block mb-2 text-success">Ezrqn kemboi, Kenya</strong>
+              <p>
+                Momi Foundation has helped me understand software development. I am confident right Now
+                that I am ready and prepared to apply for Junior to mid-level jobs. Also, apart 
+                from learning coding, I was able to learn leadership skills, project management, 
+                soft skills and general team building.
+              </p>
+              <p className="mb-1 text-muted">February 2023</p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="row p-2 border rounded flex-md-row mb-4 position-relative">
+              <strong className="d-inline-block mb-2 text-success">David Mwangi, Kenya</strong>
+              <p>
+                Momi Foundation has helped me understand software development. I am confident right Now
+                that I am ready and prepared to apply for Junior to mid-level jobs. Also, apart 
+                from learning coding, I was able to learn leadership skills, project management, 
+                soft skills and general team building.
+              </p>
+              <p className="mb-1 text-muted">February 2023</p>
+            </div>
+          </div>
+          <div className="col-md-3">
+            <div className="row p-2 border rounded flex-md-row mb-4 position-relative">
+              <strong className="d-inline-block mb-2 text-success">Mark Too, Kenya</strong>
+              <p>
+                Momi Foundation has helped me understand software development. I am confident right Now
+                that I am ready and prepared to apply for Junior to mid-level jobs. Also, apart 
+                from learning coding, I was able to learn leadership skills, project management, 
+                soft skills and general team building.
+              </p>
+              <p className="mb-1 text-muted">February 2023</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className='container-fluid bg-light mb-4 p-4'
+      >
+        <h3 className='text-center pt-2 text-success'>Get In Touch</h3>
+        <p className='text-center pt-2'>
+          We are here to help you progress in your career
+        </p>
+        <Link className='d-flex justify-content-center col-12' to='/register-software'>
+          <button 
+            type='button'
+            className='btn btn-success col-2'
+          >
+            Register Now
+          </button>
+        </Link>
       </div>
       <Footer />
     </div>
